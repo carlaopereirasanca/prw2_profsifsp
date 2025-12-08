5c4ea0effc5ea691b56e5c394dbe0e315bd2c55e
Carlos Jose Pereira <carlaopereirasanca@gmail.com>
2025-12-08 14:07:20 -0300

    apos a aula 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,46 @@
 
-import './App.css'
-import Banner from './componentes/Banner/Banner';
-import Formulario from './componentes/Formulario/Formulario';
+import React, { useState } from "react";
+import Banner from "./componentes/Banner/Banner";
+import Formulario from "./componentes/Formulario/Formulario";
+import Lista from "./componentes/Lista/Lista";
 
 function App() {
 
-  return (
+	// lista (array) de professores.
+	// o valor inicial é um array vazio []
+	const [professores, setProfessores] = useState( [] )
 
-    <div>
+  const adicionaProf = (prof) => {
 
-      <Banner />
-      <Formulario />
+    // criando uma nova lista com o conteúdo anterior da lista.
+    //
+    // CUIDADO!!! não vá fazer listaNova = professores,
+    // isso não criará uma nova lista, apenas criará uma nova
+    // referência (ponteiro) para a mesma lista (o mesmo objeto).
+    //
+    // O método slice() de um array em JavaScript 
+    // retorna uma cópia superficial de uma porção de um array 
+    // dentro de um novo array objeto selecionado desde start 
+    // até end (end não é incluído). 
+    // Se não passarmos nenhum argumento para slice(), 
+    // ele retorna uma cópia do array inteiro.
+    const listaNova = professores.slice();
 
-    </div>
-  );
+    // adicionando o novo professor nesssa lista:
+    listaNova.push(prof)
 
+    // colocando a nova lista atualizada no estado:
+    setProfessores( listaNova )
+  }
+
+	return (
+		<div>
+			<Banner />
+			<Formulario aoProfCadastrado={prof => adicionaProf(prof)} />
+      <Lista objetos={professores} />
+		</div>
+	);
 }
 
-export default App
+export default App;
+
--- src/componentes/Formulario/Formulario.jsx
+++ src/componentes/Formulario/Formulario.jsx
@@ -1,20 +1,77 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './Formulario.css';
 import CampoTexto from "../CampoTexto/CampoTexto";
+import ListaSuspensa from "../ListaSuspensa/ListaSuspensa";
+import Botao from "../Botao/Botao";
 
-const Formulario = () => {
+const Formulario = (props) => {
 
-	return (
-		<section className="formulario">
-			<form>
-				<h2>Dados do professor:</h2>
-				<CampoTexto label="Nome" placeholder="Digite seu nome" />
-				<CampoTexto label="Título" placeholder="Digite seu título" />
-				<CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
-			</form>
-		</section>
-	)
+    const areas = [
+        'Progr. Básica',
+        'Progr. Web',
+        'Banco de Dados',
+        'Diversos'
+    ]
+
+    // Criando os estados...
+    const [nome, setNome] = useState('')
+    const [titulo, setTitulo] = useState('')
+    const [imagem, setImagem] = useState('')
+    const [area, setArea] = useState('Progr. Básica')
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault()
+        props.aoProfCadastrado(
+            {
+                "nome" : nome,
+                "titulo" : titulo,
+                "imagem" : imagem,
+                "area" : area
+            }
+        );
+    }
+
+    return (
+        <section className="formulario">
+
+            <form onSubmit={aoSalvar}>
+
+                <h2>Dados do professor:</h2>
+
+                <CampoTexto
+                    label="Nome"
+                    placeholder="Digite seu nome"
+                    valor={nome}
+                    aoAlterado={valor => setNome(valor)}
+                />
+
+                <CampoTexto
+                    label="Título"
+                    placeholder="Digite seu título"
+                    valor={titulo}
+                    aoAlterado={valor => setTitulo(valor)}
+                />
+
+                <CampoTexto
+                    label="Imagem"
+                    placeholder="Digite o endereço da imagem"
+                    valor={imagem}
+                    aoAlterado={valor => setImagem(valor)}
+                />
+
+                <ListaSuspensa
+                    label="Áreas"
+                    itens={areas}
+                    aoAlterado={valor => setArea(valor)}
+                />
+
+                <Botao>Criar Professor</Botao>
+
+            </form>
+
+        </section>
+    )
 }
 
 export default Formulario;
